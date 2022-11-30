21513b9bf6c9b462c72fb3e1a7dfafb0ebdde039
Adwait <adwait.kul.2018@gmail.com>
2022-11-29 17:09:36 -0800

    Added metres

--- map.py
+++ map.py
@@ -4,10 +4,11 @@ import pandas
 file_name = pandas.read_csv("volcanoes.txt")
 latitude_list = list(file_name["LAT"])
 longitude_list = list(file_name["LON"])
+elevation_list = list(file_name["ELEV"])
 
 map = folium.Map(location=[49.282, -123.120], tiles="Stamen Terrain")
 feature_group = folium.FeatureGroup(name="Map")
-for lat, lon in zip(latitude_list, longitude_list):
-    feature_group.add_child(folium.Marker(location=[lat, lon], popup="Marker", icon=folium.Icon(color="green")))
+for lat, lon, el in zip(latitude_list, longitude_list, elevation_list):
+    feature_group.add_child(folium.Marker(location=[lat, lon], popup=str(el) + " m", icon=folium.Icon(color="green")))
     map.add_child(feature_group)
 map.save("Map.html")
